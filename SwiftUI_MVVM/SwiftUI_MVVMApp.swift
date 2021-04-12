9789c2284931199abcadfd709d26277b3815f5b8
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-12 11:30:00 +0530

    Initial Commit

---
+++ SwiftUI_MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI_MVVM
+//
+//  Created by Knoxpo MacBook Pro on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
